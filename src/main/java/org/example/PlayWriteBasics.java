fe5d635fd2f91670d50d7d6ce4490651b0c16394
kkrajeev999 <kkrajeev999@gmail.com>
2026-05-03 13:44:31 +0530

    Playwright_launching_browser

---
+++ src/main/java/org/example/PlayWriteBasics.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class PlayWriteBasics {
+    public static void main(String[] args) {
+        Playwright playwright = Playwright.create();
+        /*Browser browser = playwright.chromium().launch(
+        new BrowserType().launchOptions().setHeadless(false)
+        );*/
+        Browser browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+
+        /*browser.newPage();*/
+        Page page = browser.newPage();
+        page.navigate("https://www.amazon.in/?");
+        String pagetitle = page.title();
+        System.out.println("Page Title is: " + pagetitle);
+        String pageurl = page.url();
+        System.out.println("Page URL is: " + pageurl);
+        browser.close();
+        playwright.close();
+    }
+
+
+
+}
